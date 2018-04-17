9c60bede8e8af011e29326418b1669095382deb3
Andrea Vicari <andrea.vicari@usi.ch>
2018-04-17 15:41:24 +0200

    second release

--- rchoose.rb
+++ rchoose.rb
@@ -1,8 +1,11 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Rchoose < Formula
   desc ""
   homepage ""
-  url "https://github.com/vicaran/rchoose/archive/v1.0.0.tar.gz"
-  sha256 "a703e6615f091f5dd19dd74fac36a47d2c5634fb9e6d1276a862feebc614ab57"
+  url "https://github.com/vicaran/rchoose/archive/v2.0.0.tar.gz"
+  sha256 "da4a052ab869fa4168360f494c20cff8d3dfc7b6d0125d969b2fb8c0276c7bec"
   # depends_on "cmake" => :build
 
   def install
